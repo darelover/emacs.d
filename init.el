7bf1bc9a985f3b77fb03f9af4120de58c23b7962
darelover <atul0108@outlook.com>
2021-02-17 23:25:31 +0530

    wrap around company selections

--- init.el
+++ init.el
@@ -94,6 +94,8 @@
 
 ;; company uses backends to provide completions and a frontend to visualise the list of completions suggestions
 (use-package company
+  :init
+  (setq company-selection-wrap-around t)
   :config
   ;; Zero delay when pressing tab
   (setq company-idle-delay 0)
